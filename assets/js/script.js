28f61f5868bf9a3a09898947498d310a1988118e
Cadenza <mirandalucsb@gmail.com>
2021-11-24 11:33:48 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var tasks = {};
 
 var auditTask = function(taskEl) {
   // get date from task element
+  console.log(taskEl)
   var date = $(taskEl).find("span").text().trim();
 
 
@@ -176,7 +177,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -216,15 +217,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
    // array to store the task data in
@@ -262,6 +269,11 @@ console.log(tempArr);
   }
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 
 $("#trash").droppable({
@@ -270,12 +282,15 @@ $("#trash").droppable({
   drop: function(event, ui) {
     ui.draggable.remove();
     console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
